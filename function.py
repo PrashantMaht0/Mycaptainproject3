7630482afc38042e077df9e72ba901c24e2089f3
PrashantMaht0 <119067260+PrashantMaht0@users.noreply.github.com>
2023-01-29 14:49:13 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,12 @@
+a=input("plz enter a string"+"\n")
+
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+print(most_frequent(a))
